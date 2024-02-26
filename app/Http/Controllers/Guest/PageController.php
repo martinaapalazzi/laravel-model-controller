8ef194b23049871fa15a406a23bb53b7b8c9f7ee
martinaapalazzi <martinaapalazzi@gmail.com>
2024-02-26 15:21:42 +0100

    created Controller

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -8,5 +8,9 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index() {
+
+        $movies = Movie::all();
+        return view('movies.index', compact('movies'));
+    }
 }
