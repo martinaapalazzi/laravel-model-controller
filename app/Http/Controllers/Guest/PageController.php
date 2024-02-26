c8aeb74990a6efdf877f0318860fe1bdd3752225
martinaapalazzi <martinaapalazzi@gmail.com>
2024-02-26 15:15:56 +0100

    created model

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Models\Movie;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    //
+}
